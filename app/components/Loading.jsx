556d0f5be15b5a0736a957a132bad8b92cc72352
Your Name <meejanursk@gmail.com>
2025-07-11 11:42:05 +0530

    f

--- app/components/Loading.jsx
+++ app/components/Loading.jsx
@@ -45,7 +45,7 @@ export default function Loading() {
   }, [])
 
   return (
-    <div className="h-screen duration-500 w-screen bg-white max-sm:-translate-x-[0.1vw] fixed top-0 z-[300]">
+    <div className="h-screen duration-500 w-screen bg-black max-sm:-translate-x-[0.1vw] fixed top-0 z-[300]">
       <img
         className="skelbase fixed max-sm:scale-[2] rotate-[-720deg] max-sm:translate-y-[37vh] top-[11.1vw] right-[39vw] w-[26.1vw] lg:top-[11.38vw] lg:right-[39.25vw] z-[250]"
         src="/WatchParts/skelbase.svg"
@@ -54,7 +54,7 @@ export default function Loading() {
         className="screen fixed rotate-[720deg] max-sm:scale-[2] w-[15.4vw] max-sm:translate-y-[37vh] top-[15.5vw] right-[44.70vw] z-[200] duration-700 transition-opacity"
         src="/WatchParts/skelscreen.svg"
       />
-      <h1 id="loading-count" className="fixed lg:left-[45.4vw] lg:top-[75%] top-[70%] left-[42.5%] text-2xl font-bold">
+      <h1 id="loading-count" className="fixed lg:left-[45.4vw] text-white lg:top-[75%] top-[70%] left-[42.5%] text-2xl font-bold">
         0%
       </h1>
     </div>
